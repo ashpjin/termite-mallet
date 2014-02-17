355432fb9a1b100c9de56e4612b6788e6c7f6096
Ashley Jin <ashpjin@cs.stanford.edu>
2014-02-17 03:09:37 -0800

    testing out web2py app later instead of exhaustive

--- server_src/modules/corpus.py
+++ server_src/modules/corpus.py
@@ -2,6 +2,10 @@
 
 import os
 import json
+from gluon import DAL
+from gluon import Field
+
+import time
 
 class Corpus:
 	def __init__( self, request ):
@@ -41,24 +45,56 @@ class Corpus:
 		searchText = params["searchText"]
 		searchLimit = params["searchLimit"]
 		searchOffset = params["searchOffset"]
-
+		
+		print "web2py"
+		start = time.clock()
+		
+		# get data using web2py application layer
+		db = DAL('sqlite://doc-meta.sqlite', migrate_enabled=False)
+		db.define_table('DocMeta', Field('Key'), Field('DocContent'), Field('DocID'), primarykey=['Key'])
+		
+		# build query using params
+		q = db.DocMeta.DocContent.like('%' + searchText + '%')
+		matches = db(q).count();
+		limitb = searchOffset + searchLimit
+		
+		count = 0
+		pyResults = {}
+		queryResults = db(q).select(orderby=db.DocMeta.DocID, orderby_on_limitby = False, limitby=(searchOffset, limitb));
+		for row in queryResults:
+		    count += 1
+		    pyResults[row.DocID] = dict(row)
+		
+		elapsed = (time.clock() -start)
+		print "elapsed = ", elapsed
+		        
+		        
+		print "exhaustive"
+		start = time.clock()
+		# get data using exhaustive search of JSON
 		filename = os.path.join( self.request.folder, 'data/corpus', 'doc-meta.json' )
 		with open( filename ) as f:
 			content = json.load( f, encoding = 'utf-8' )
 			results = {}
 			matchCount = 0
+			discardCount = 0
 			keys = sorted(content.keys())
 			for index in range(len(keys)):
 			    obj = content[keys[index]]
 			    docContent = obj["DocContent"]
 			    if searchText in docContent:
 			        matchCount += 1
-			        if len(results) < searchLimit and index >= searchOffset:
-			           results[obj["DocID"]] = obj
+			        if len(results.keys()) < searchLimit and discardCount >= searchOffset:
+			            results[obj["DocID"]] = obj
+			        elif discardCount < searchOffset:
+			            discardCount += 1
+		elapsed = (time.clock() - start)
+		print "elapsed = ", elapsed
+		
 		return {
-			"Documents" : results,
-			"docCount" : len(results),
-			"docMaxCount" : matchCount
+			"Documents" : pyResults,
+			"docCount" : len(pyResults),
+			"docMaxCount" : matches
 		}
 
 	def GetTermFreqs( self, params = None ):
